0c23a228f1a461224bd6cb780aa2989c30d021a1
Nicolas LORION <nlorion.mail@gmail.com>
2022-05-09 16:19:22 +0200

    not fixed yet

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: nlorion <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 16:54:02 by nlorion           #+#    #+#             */
-/*   Updated: 2022/05/06 17:31:27 by nlorion          ###   ########.fr       */
+/*   Updated: 2022/05/09 16:12:45 by nlorion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,26 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	char	*sc;
 	char	*c;
-	size_t	slen;
+	size_t	llen;
 
 	sc = (char *)big;
 	c = (char *)little;
 	if (*c++ != '\0')
 	{
-		slen = ft_strlen(little);
-		while (strncmp(big, little, slen) != 0)
-		{
+		llen = ft_strlen(little);
+		while (strncmp(big, little, llen) != 0)
+		{	
 			while (sc != c)
 			{
 				if (*sc++ == '\0' || len-- < 1)
 					return (NULL);
 			}
-			if (len > slen)
+			if (len > llen)
 				return (NULL);
+			if (*sc == 0 || *c == 0)
+				return ((char *)big);
+			big--;
 		}
-		sc--;
 	}
 	return ((char *)big);
 }
