a575b5bd2078948e9b07543e1df5c746f2806e99
Nicolas LORION <nlorion.mail@gmail.com>
2022-05-09 19:25:11 +0200

    fixed indentation

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: nlorion <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 19:24:50 by nlorion           #+#    #+#             */
-/*   Updated: 2022/05/06 18:08:21 by nlorion          ###   ########.fr       */
+/*   Updated: 2022/05/09 18:19:11 by nlorion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,13 @@
 
 char	*ft_strdup(const char *str)
 {
-	int		len;
-	char	*copy;
+	size_t		len;
+	char		*copy;
 
-	len = 0;
-	while (str[len])
-		len++;
-	copy = (char *)malloc(sizeof(*str) * (len + 1));
+	len = ft_strlen(str) + 1;
+	copy = malloc(len);
 	if (!copy)
 		return (NULL);
-	while (*str)
-		*copy++ = *str++;
-	*copy = '\0';
+	ft_memcpy(copy, str, len);
 	return (copy);
 }
-/*
-int     main (void)
-{
-    char    s1[20];
-    char    s2[20] = "Bonjour";
-
-    printf("%s\n", ft_strdup(s2));
-}
-*/
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: nlorion <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 17:23:46 by nlorion           #+#    #+#             */
-/*   Updated: 2022/05/06 17:33:20 by nlorion          ###   ########.fr       */
+/*   Updated: 2022/05/09 18:20:09 by nlorion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned char *str1;
-	unsigned char *str2;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
