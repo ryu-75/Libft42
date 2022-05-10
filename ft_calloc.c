eb2e75950c10f45f022ff0f679c9b6b4e60f0632
Nicolas LORION <nlorion.mail@gmail.com>
2022-05-10 17:12:55 +0200

    fixed mallo error

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: nlorion <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 16:59:02 by nlorion           #+#    #+#             */
-/*   Updated: 2022/05/09 12:33:06 by nlorion          ###   ########.fr       */
+/*   Updated: 2022/05/10 14:04:47 by nlorion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,12 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
-
-	ptr = malloc(nmemb * size);
+	
 	if (nmemb == 0 || size == 0)
-	{
 		return (NULL);
-	}
 	if (nmemb == SIZE_MAX && size == SIZE_MAX)
 		return (NULL);
+	ptr = malloc(nmemb * size);
 	if (!ptr)
 		return (ptr);
 	ft_bzero(ptr, nmemb * size);
