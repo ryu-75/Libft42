0f7038cbe00f13eafc5ed82bac7d7369356c4ca9
Nicolas LORION <nlorion.mail@gmail.com>
2022-05-11 15:57:21 +0200

    fixed split

--- ft_split.c
+++ ft_split.c
@@ -6,138 +6,105 @@
 /*   By: nlorion <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 17:58:15 by nlorion           #+#    #+#             */
-/*   Updated: 2022/05/11 13:01:12 by nlorion          ###   ########.fr       */
+/*   Updated: 2022/05/11 15:56:34 by nlorion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_freeTab(char **str)
+static int	ft_freeTab(char **str, size_t size)
 {
-	size_t	i;	
-
-	i = 0;
-	while (str[i] != NULL)
-		free(str[i]);
-		i++;
-	free(str);
+	while (size--)
+		free(str[size]);
+	return (-1);
 }
 
-int	ft_countword(char const *s, char const set)
+static int	ft_countword(char const *s, char set)
 {
 	size_t	i;
-	size_t	len_s;
 	size_t	word;
 
 	i = 0;
 	word = 0;	
-	len_s = ft_strlen(s);
 	while (s[i] != '\0')
 	{
-		if (s[i] == set && s[i + 1] != set)
-			word++;
+		if ((s[i + 1] == set || s[i + 1] == '\0') == 1
+			&& (s[i] == set || s[i] == '\0') == 0)
+		word++;
 		i++;
 	}
-	word++;
 	return (word);
 }
 
-char	*ft_writeword(char *dest, char *src, char const set)
+static void	ft_writeword(char *dest, char const *src, char set)
 {
 	size_t	i;
 
 	i = 0;
-	while (src[i] != '\0')
+	while ((src[i] == set || src[i] == '\0') == 0)
 	{
-		if (dest[i] != src[i] && src[i] != set)
-		{
-			dest[i] = src[i];
-		}
+		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-	return (dest);
 }
 
-char	**ft_splitword(char *s, char set)
+static int	ft_splitword(char **tab, char const *str, char set)
 {
-	char	**tab;
-	size_t	i;
-	size_t	len;
-
-	len = ft_strlen(s);
-	tab = malloc((len + 1) * sizeof(char));
-	while (s[i] != '\0')
-	{
-		if (ft_writeword(&s[i + 1], &s[i], set) == 0)
-			i++;
-		if (ft_writeword(&s[i + 1], &s[i], set))
-		{
-			tab[i + 1] = &s[i];
-		}
-		i++;
-	}
-	return (tab);
-}
-/*
-char	**ft_split(char const *s, char const c)
-{
-	char	**tab;
-	size_t	len;
 	size_t	i;
 	size_t	j;
-	size_t	k;
+	size_t	word;
 
 	i = 0;
-	len = ft_strlen(s);
-	tab = malloc((len + 1) * sizeof(char));
-	while (i <= len)
+	word = 0;
+	while (str[i] != '\0')
 	{
-		tab[i] = malloc((len + 1) * sizeof(char *));
-		i++;	
-	}
-	i = 0;
-	if (!tab)
-		return (NULL);
-	while (s[i])
-	{
-		while (ft_strsearch(&c, s[i]) && s[i])
+		if ((str[i] == set || str[i] == '\0') == 1)
 			i++;
-		while (ft_strsearch(&c, s[i]) && s[i])
+		else
 		{
-			tab[j][k] = s[i];
-			i++;
-			k++;
+			j = 0;
+			while ((str[i + j] == set) || (str[i + j] == '\0') == 0)
+				j++;
+			tab[word] = (char *)malloc(sizeof(char) * (j + 1));
+			if (!tab[word])
+				return (ft_freeTab(tab, word - 1));
+			ft_writeword(tab[word], str + i, set);
+			i += j;
+			word++;
 		}
-		while (tab[j][k] == '\0')
-			k = '\0';
-		while (tab[j][k] != '\0')
-			j++;
 	}
-	tab[i] = '\0';
-	freeTab(tab);
+	return (0);
+}
+char	**ft_split(char const *s, char c)
+{
+	char	**tab;
+	size_t	word;
+
+	word = ft_countword(s, c);
+	tab = (char **)malloc(sizeof(char *) * (word + 1));
+	if (!(tab))
+		return (NULL);
+	tab[word] = 0;
+	if (ft_splitword(tab, s, c) == -1)
+		return (NULL);
 	return (tab);
 }
-*/
+
 int main(int argc, char **argv)
 {
-	/*
+	
+	//char	*test = "bonjour, je suis, la";
     	size_t	i;
-	char	s = ',';
-	i = 0;
+
+	i = 1;
 	if (argc > 1)
 	{
 		while (argv[i])
 		{
-        		printf("%s", *ft_split(argv[i], s));
+        		printf("%s", *ft_split(argv[i], ','));
 			i++;
 		}
 	}
-	*/
-	char	*test = "bonjour, je suis, la";
-	char	set = ',';
-
-	printf("%s", *ft_splitword(test, set));	
     	return (0);
 }
-
