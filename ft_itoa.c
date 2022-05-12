07611cb42de7301f7c2dcf764c13e9915df2a9a4
Nicolas LORION <nlorion.mail@gmail.com>
2022-05-12 18:38:04 +0200

    not fixed yet itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,17 +6,30 @@
 /*   By: nlorion <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 16:22:38 by nlorion           #+#    #+#             */
-/*   Updated: 2022/05/12 17:30:20 by nlorion          ###   ########.fr       */
+/*   Updated: 2022/05/12 18:36:43 by nlorion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_countnum(size_t n)
+static void	ft_putnbr(long n, char *s, int *i)
+{
+	if (n > 9)
+	{
+		ft_putnbr(n % 10, s, i);
+		ft_putnbr(n / 10, s, i);	
+	}
+	else 
+		s[(*i)++] = n + '0';
+}
+
+static size_t	ft_countnum(long long n)
 {
 	int	res;
 
 	res = 0;
+	if (n < 0)
+		res++;
 	if (n < 10)
 		return (1);
 	while (n != 0)
@@ -31,34 +44,27 @@ char	*ft_itoa(int n)
 {
 	char	*tab;
 	int	count;
-	char	val;
 	int	neg;
 	int	i;
+	long	nbr;
 
+	nbr = n;
 	i = 0;	
 	neg = 0;
-	val = 0;
-	count = ft_countnum(n);
+	count = ft_countnum(nbr);
 	tab = malloc(sizeof(char) * count + 1);
+	if (!tab)
+		return (NULL);
 	if (n == 0)
 		tab[i++] = '0';	
 	if (n < 0)
 	{
 		neg = 1;
-		n = -n;
-	}
-	while (n != 10)
-	{
-		val = n % 10;
-		if (val > 9)
-			tab[i++] = (val - 9) + 48;
-		else 
-			tab[i++] = val + 48;
+		nbr = -nbr;
 	}
 	if (neg == 1)
 		tab[i++] = '-';
-	if (!tab)
-		return (NULL);
+	ft_putnbr(nbr, tab, &i);
 	tab[i] = '\0';
 	return (tab);
 }
@@ -66,7 +72,7 @@ char	*ft_itoa(int n)
 int	main(void)
 {
 	
-	int	n = -5;
+	int	n = -2147483648;
 	/*
 	int	count = ft_countnum(n);
 	char	*tab;
