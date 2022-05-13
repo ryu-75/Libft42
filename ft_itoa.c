bcfc9cffef07b3668c15d991abd674f4ec6c2a75
Nicolas LORION <nlorion.mail@gmail.com>
2022-05-13 18:42:39 +0200

    itoa OK

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,20 +6,21 @@
 /*   By: nlorion <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 16:22:38 by nlorion           #+#    #+#             */
-/*   Updated: 2022/05/12 18:36:43 by nlorion          ###   ########.fr       */
+/*   Updated: 2022/05/13 15:08:21 by nlorion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
 
 static void	ft_putnbr(long n, char *s, int *i)
 {
 	if (n > 9)
 	{
+		ft_putnbr(n / 10, s, i);
 		ft_putnbr(n % 10, s, i);
-		ft_putnbr(n / 10, s, i);	
 	}
-	else 
+	else
 		s[(*i)++] = n + '0';
 }
 
@@ -27,12 +28,13 @@ static size_t	ft_countnum(long long n)
 {
 	int	res;
 
-	res = 0;
+	res = 1;
 	if (n < 0)
+	{
+		n *= -1;
 		res++;
-	if (n < 10)
-		return (1);
-	while (n != 0)
+	}
+	while (n > 9)
 	{
 		n /= 10;
 		res++;
@@ -43,55 +45,26 @@ static size_t	ft_countnum(long long n)
 char	*ft_itoa(int n)
 {
 	char	*tab;
-	int	count;
-	int	neg;
-	int	i;
+	int		count;
+	int		neg;
+	int		i;
 	long	nbr;
 
-	nbr = n;
-	i = 0;	
+	i = 0;
 	neg = 0;
+	nbr = n;
 	count = ft_countnum(nbr);
-	tab = malloc(sizeof(char) * count + 1);
+	tab = (char *)malloc(sizeof(char) * count + 1);
 	if (!tab)
 		return (NULL);
-	if (n == 0)
-		tab[i++] = '0';	
 	if (n < 0)
 	{
 		neg = 1;
-		nbr = -nbr;
-	}
+		nbr *= -1;
+	}	
 	if (neg == 1)
 		tab[i++] = '-';
 	ft_putnbr(nbr, tab, &i);
 	tab[i] = '\0';
 	return (tab);
-}
-
-int	main(void)
-{
-	
-	int	n = -2147483648;
-	/*
-	int	count = ft_countnum(n);
-	char	*tab;
-	int	val;
-	int	i = 0;
-
-	val = 0;
-	tab = malloc(sizeof(char) * count + 1);
-	while (n != 0)
-	{
-		val = n;
-		val %= 10;
-		val /= 10;
-		if (val > 9)
-			tab[i++] = (val - 9) + 48;
-		else
-			tab[i++] = val + 48;
-	}
-	tab[i] = '\0';
-	*/
-	printf("%s", ft_itoa(n));
 }
