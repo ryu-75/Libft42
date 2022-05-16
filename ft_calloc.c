04888c418aa6290394a1e0da52e5a63b8f4bd16b
Nicolas LORION <nlorion.mail@gmail.com>
2022-05-16 20:24:55 +0200

    add condition

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: nlorion <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 16:59:02 by nlorion           #+#    #+#             */
-/*   Updated: 2022/05/10 14:04:47 by nlorion          ###   ########.fr       */
+/*   Updated: 2022/05/16 20:15:46 by nlorion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
-	
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	if (nmemb == SIZE_MAX && size == SIZE_MAX)
+		return (NULL);
+	if ((nmemb * size / size) == SIZE_MAX)
 		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (!ptr)
